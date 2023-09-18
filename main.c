3f8e3a05a0b62b166902c731e5b09e167d8aecb8
Anastasiiasyvak <asyvak@kse.org.ua>
2023-09-19 00:24:39 +0300

    first part

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int command;
+    char* File;
+    char text[100];
+    File = (char* )malloc(sizeof(char));
+
+    while(true) {
+        printf("Choose the command! \n");
+        scanf("%d", &command);
+        getchar();
+        switch (command) {
+            case 1:
+                printf("Enter text to append:", text);
+                fgets(text, sizeof(text), stdin);
+                File = (char *) realloc(File, strlen(File) + strlen(text) + 1);
+                strcat(File, text);
+                printf("%s", File);
+                break;
+        }
+    }
 }
