16013fe8dc4d71b8144d67e4c78adef6eb95c1fb
Anastasiiasyvak <asyvak@kse.org.ua>
2023-09-19 14:05:02 +0300

    some changes + default case

--- main.c
+++ main.c
@@ -4,26 +4,36 @@
 
 int main() {
     int command;
-    char* File;
-    char text[100];
-    File = (char* )malloc(sizeof(char));
-
+    char* File = NULL;
     while(true) {
         printf("Choose the command! \n");
         scanf("%d", &command);
         getchar();
         switch (command) {
             case 1:
-                printf("Enter text to append: \n", text);
+                printf("Enter text to append: \n");
+                char text[100];
                 fgets(text, sizeof(text), stdin);
-                File = (char *) realloc(File, strlen(File) + strlen(text) + 1);
-                strcat(File, text);
+                size_t sizeText = strlen(text);
+                size_t sizeFile = (File == NULL) ? 0: strlen(File);
+                size_t sizeFileNow = sizeText + sizeFile + 1;
+                char* newFile = (char*)malloc(sizeFileNow);
+                if (File != NULL) {
+                    strcpy(newFile, File);
+                }
+                strcat(newFile, text);
+                File = newFile;
                 printf("%s", File);
                 break;
             case 2:
                 strcat(File, "\n");
                 printf("New line is started\n");
                 break;
+
+            default:
+                printf("The command is not implemented\n");
+                break;
+            }
         }
     }
-}
+
